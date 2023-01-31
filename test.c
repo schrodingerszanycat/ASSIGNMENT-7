a6755e137390b7d7f5e5d7a4f33bf9abf75b5524
schrodingerszanycat <x.y@z>
2023-02-01 02:30:33 +0530

    Add code

---
+++ test.c
@@ -0,0 +1,17 @@
+/*Write a program in C to read n int values in an array and display it in reverse order.*/
+#include <stdio.h>
+
+int main() {
+    int size = 0;    
+    printf("Enter the size of the array: \n");   // To create the array
+    scanf("%d", &size);
+    printf("Enter the elements: ");
+    int arr[size];
+    for (int i = 0; i<size; i++) {
+        scanf("%d ", &arr[i]);
+    }  
+
+    for (int j = size-1; j >=0 ; j--) {
+        printf("%d ", arr[j]);
+    }
+}
