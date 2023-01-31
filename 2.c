78d19cebd47de1ef1c29f4bd1f977c1e5b413bb4
schrodingerszanycat <x.y@z>
2023-02-01 00:04:01 +0530

    Add code

---
+++ 2.c
@@ -0,0 +1,16 @@
+/*Write a function in C to find the sum of all array elements.*/
+#include <stdio.h>
+
+int main() {
+    int size = 0;
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
+
+    int arr[size];
+    printf("Enter the elements: ");
+    for(int i = 0; i < size; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    for(int j = 0; j<size; )
+}
