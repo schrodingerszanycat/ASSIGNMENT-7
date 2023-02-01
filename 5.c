a3396091a155d9b3b181855f06ab28174a4d55d8
schrodingerszanycat <x.y@z>
2023-02-01 12:17:26 +0530

    Add code

--- 5.c
+++ 5.c
@@ -1,3 +1,32 @@
 /*Write a function in C to count a total number of duplicate elements in an array.*/
 #include <stdio.h>
 #include <stdlib.h>
+
+int freq(int *a, int n) {
+    int visit[n];
+    for (int i = 0; i<n; i++) {
+        if (visit[i] == -1)
+            continue;
+        int count = 1;
+        for (int j = i+1; j<n; j++) {
+            if (a[j] == a[i]){
+                count++;
+                visit[j] = -1;
+            }
+        }
+    printf("The frequency of %d is %d.\n", a[i], count);
+    }
+}
+
+int main() {
+    int size = 0;
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
+    int arr[size];
+    int* p = (int *) malloc(size*sizeof(int));
+    for (int j = 0; j<size; j++) 
+        scanf("%d", &arr[j]);
+
+    freq(arr, size);
+
+}
