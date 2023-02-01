7d032d1af1cf78458bde9310af20d58b7bf0e05e
schrodingerszanycat <x.y@z>
2023-02-01 11:51:50 +0530

    Add code

--- test.c
+++ test.c
@@ -16,12 +16,12 @@ int main() {
     }
 }
 
-
-for (int i = 0; i<n; i++) {
-    if (!flag[i]){
-        for (int j = 0; j<n; j++) {
-            flag[j] = a[i] == a[j];
-            flag[i] = flag[i] || flag[j];
-        }
-    }
-}
+// KAUSTAV SIR'S FREQUENCY CODE
+// for (int i = 0; i<n; i++) {                      
+//     if (!flag[i]){
+//         for (int j = 0; j<n; j++) {
+//             flag[j] = a[i] == a[j];
+//             flag[i] = flag[i] || flag[j];
+//         }
+//     }
+// }
