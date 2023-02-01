d05690fa2ee6f435fd070b676241173485104690
schrodingerszanycat <x.y@z>
2023-02-01 12:26:12 +0530

    Add code

---
+++ v.c
@@ -0,0 +1,10 @@
+// KAUSTAV SIR'S FREQUENCY CODE
+// for (int i = 0; i<n; i++) {                      
+//     if (!flag[i]){
+//         for (int j = 0; j<n; j++) {
+//             flag[j] = a[i] == a[j];
+//             flag[i] = flag[i] || flag[j];
+//         }
+//     }
+// }
+
