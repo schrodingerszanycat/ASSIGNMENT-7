b7261ef1c96ed99f20c8494c551e8b8f09156a4d
schrodingerszanycat <x.y@z>
2023-02-05 07:45:10 +0530

    Add code

--- 14.c
+++ 14.c
@@ -2,19 +2,9 @@
 #include <stdio.h>
 
 int main() {
-    int size = 0;
-    printf("Enter the size of the array: ");
-    scanf("%d", &size);
-
-    int arr[size];
-    printf("Enter all the elements: ");
-    for (int i = 0; i<size; i++) {
-        scanf("%d", &arr[i]);
-    }
-    int sec_max = arr[0];
-    for (int j = 1; j<size; j++) {
-        if (sec_max < arr[j]) sec_max = arr[j-1];
-        else continue;
-    }
-    printf("%d\n", sec_max);
+    
 }
+// sort the whole array in decreasing order, then pick second largest
+// traverse the whole arrar twice, the second time excluding the max one then pick the largest
+// using two terms and seeing if it is between the two or not
+
