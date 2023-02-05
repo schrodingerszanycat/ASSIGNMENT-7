5bf009c68336c32bff3d1220a1f50dc61bc8d80b
schrodingerszanycat <x.y@z>
2023-02-05 08:43:17 +0530

    Add code

--- 17.c
+++ 17.c
@@ -1,13 +1,30 @@
 /*Write a function in C for the multiplication of two matrices.*/
 #include <stdio.h>
 
-int addMat(int *a, int *b, int size);
+int addMat(int a[3][3], int b[3][3]);
 
 int main() {
-    int a[][3] = {1,2,3,4,5,6,7,8,9};
-    int b[][3] = {9,8,7,6,5,4,3,2,1};
+    int a[3][3] = {{2,1,1},
+                   {1,1,1}, 
+                   {1,1,1}};
+    int b[3][3] = {{1,1,1},
+                   {1,1,1},
+                   {1,1,1}};
     addMat(a, b);
 }
-int addMat(int *a, int *b, int size) {
-    
+int addMat(int a[3][3], int b[3][3]) {
+    int c[3][3];
+    for (int i = 0; i<3; i++) {
+        for (int j = 0; j<3; j++) {
+            c[i][j] = 0;
+            for (int k = 0; k<3; k++) {
+                c[i][j] =+ (a[i][k])*(b[k][j]);
+            }
+        }
+    }
+    for (int p = 0; p<3; p++) {
+        for (int q = 0; q<3; q++) {
+            printf("%d ", c[p][q]);
+        }
+    }
 }
