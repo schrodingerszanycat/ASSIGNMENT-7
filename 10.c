b7337fc39af7cfa0b954d5fa83384f7541a11e35
schrodingerszanycat <x.y@z>
2023-02-05 07:12:13 +0530

    Add code

--- 10.c
+++ 10.c
@@ -11,17 +11,28 @@ int main() {
     scanf("%d", &size);
     int arr[size];
     printf("Enter the elements of the array: ");
-    for (int i = 0; i<size; i++) scanf("%d", &a[i]);
+    for (int i = 0; i<size; i++) scanf("%d", &arr[i]);
 
     toSeparate(arr, size);
 }
 
 int toSeparate(int *a, int size) {
-    for (int i = 0; i<size; i++) {
-        if (a[i] %2 == 0) {
-            even_array[i1] = a[i];
-            
-        }
-    }
+    int count = 0; // Of odd integers
+    for (int i = 0; i<size; i++)
+        if (a[i] % 2) count++;
     
+    int odd_array[count]; int even_array[size-count];
+
+    int i1 = 0, i2 = 0; // For odd and even arrays respectively
+    for (int j = 0; j<size; j++) {
+        if (a[j] % 2) {
+            odd_array[i1++] = a[j];
+        }
+        else 
+            even_array[i2++] = a[j];
+    }
+    for (int p = 0; p<size; p++) printf("%d ", odd_array[p]);
+    for (int q = 0; q<size-count; q++) printf("%d ", evem_array[q]);
+
+
 }
