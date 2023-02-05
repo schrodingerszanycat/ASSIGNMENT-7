d94691f390480483f750b2c022af7e5e043b9061
schrodingerszanycat <x.y@z>
2023-02-05 08:48:23 +0530

    Add code

---
+++ 15.c
@@ -0,0 +1,27 @@
+/*Write a function in C for the addition of two matrices of the same size.*/
+#include <stdio.h>
+
+int addMat(int a[3][3], int b[3][3]);
+
+int main() {
+    int a[3][3] = {{1,1,1},
+                   {1,1,1}, 
+                   {1,1,1}};
+    int b[3][3] = {{1,4,1},
+                   {1,4,1},
+                   {1,5,1}};
+    addMat(a, b);
+}
+int addMat(int a[3][3], int b[3][3]) {
+    int c[3][3];
+    for (int i = 0; i<3; i++) {
+        for (int j = 0; j<3; j++) {
+            c[i][j] = a[i][j] + b[i][j];        
+        }
+    }
+    for (int p = 0; p<3; p++) {
+        for (int q = 0; q<3; q++) {
+            printf("%d ", c[p][q]);
+        }
+    }
+}
