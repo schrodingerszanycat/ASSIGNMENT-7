f99c11dd54d723e0597eb877c86937c94d798d1c
schrodingerszanycat <x.y@z>
2023-02-05 09:49:39 +0530

    Add code

---
+++ 11.c
@@ -0,0 +1,35 @@
+/*Write a function in C to delete an element at the desired position from an array,
+considering it a list.*/
+#include <stdio.h>
+
+int del_ele(int *arr, int size, int elmt) {
+    int p, pos = 0;
+    for (int i = 0; i<size; i++) {  // To check if element exists
+        if (arr[p] == elmt)
+            pos = p;
+    }
+    int mod_array[size-1];
+    for (int j = pos; j < size-1; j++) 
+        mod_array[j] = arr[j+1];
+    
+    for (int k = 0; k<size-1; k++) printf("%d ", arr[k]);
+}
+
+int main() {
+    int size = 0;                                      // To create Array
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
+    int arr[size];
+    printf("Enter all the elements: ");
+    for (int i = 0; i<size; i++) 
+        scanf("%d", &arr[i]); 
+    int elmt = 0;
+    printf("Enter the element you want to delete: ");
+    scanf("%d", &elmt);
+
+    printf("Original array: ");
+    for (int i = 0; i<size; i++) printf("%d ", arr[i]);
+    printf("Modified array: ");
+    del_ele(arr, size, elmt);
+}
+
