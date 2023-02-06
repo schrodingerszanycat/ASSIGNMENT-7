ffaca57814a608fc3d6692bf2dd1dbe618945a7c
schrodingerszanycat <121005817+schrodingerszanycat@users.noreply.github.com>
2023-02-06 15:39:12 +0530

    Add code

--- notdone19.c
+++ notdone19.c
@@ -2,5 +2,34 @@
 #include <stdio.h>
 
 int main() {
+    int rows = 0;
+    int columns = 0;
+    printf("Enter the number of rows and columns of the matrix: ");
+    scanf("%d %d", &rows, &columns);
+    int arr[rows][columns];
     
+    printf("Enter all the elements: ");
+    for (int i = 0; i<rows; i++) {
+        for (int j = 0; j<columns; j++)
+                scanf("%d", &arr[i][j]);
+    }
+    // to print the matrix
+    printf("Th matrix is: ");
+    for (int i = 0; i<rows; i++) {
+        for (int j = 0; j<columns; j++) {
+            printf("%d ", arr[i][j]);
+            
+        }             
+    }
+    print("\n");
+}
+
+int sum(int *arr, int rows, int columns) {
+    int sum = 0;
+    for (int i = 0; i<rows; i++) {
+        for (int j = 0; j<columns; j++) {
+            sum += arr[i][j];
+        }
+        print("Sum of row %d: %d", i+1, sum);
+    }
 }
