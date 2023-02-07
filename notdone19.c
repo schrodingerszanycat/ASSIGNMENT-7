1021aec30b24152706b84eb0301e42eded42f5ed
schrodingerszanycat <x.y@z>
2023-02-07 21:29:50 +0530

    add code

--- notdone19.c
+++ notdone19.c
@@ -28,7 +28,7 @@ int sum(int *arr, int rows, int columns) {
     int sum = 0;
     for (int i = 0; i<rows; i++) {
         for (int j = 0; j<columns; j++) {
-            sum += arr[i][j];
+            //sum += arr[i][j];
         }
         print("Sum of row %d: %d", i+1, sum);
     }
