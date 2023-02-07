eff55d6893f1ac69ab7f1fd7af7e221d9f5955e6
schrodingerszanycat <x.y@z>
2023-02-07 21:30:08 +0530

    add

---
+++ 13.c
@@ -0,0 +1,33 @@
+/*Write a function in C to add a new value in a sorted array. After the operation, the
+modified array needs to be sorted.*/
+#include <stdio.h>
+
+
+
+int new_array(int *arr, int size, int ele) {
+    arr[size] = ele;
+    
+    //for (int j = 0; j<)
+
+}    
+
+int main() {
+    int ele, size = 0;
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
+    int arr[size];
+    printf("Enter the elements of the array: ");
+    for (int i = 0; i<size; i++) 
+        scanf("%d", &arr[i]);
+    printf("Enter element to be added: ");
+    scanf("%d", &ele);
+
+    printf("Original array is: ");
+    for (int j = 0; j<size; j++) printf("%d ", arr[j]);
+    printf("\n");
+    printf("Modified array is: ");
+    for (int k = 0; k<size+1; k++) printf("%d ", arr[k]);
+    printf("\n");
+ 
+    new_array(arr, size, ele);
+}   
