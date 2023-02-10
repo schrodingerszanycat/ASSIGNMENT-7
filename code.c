8f6a2ae777b6e0bdf36dfea206fbc6e37e7982bb
schrodingerszanycat <121005817+schrodingerszanycat@users.noreply.github.com>
2023-02-10 06:54:39 +0530

    Create code.c

---
+++ code.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int add(int a, int b) {
+  return a+b;
+}
+
+int sum(int a, int b) {
+  return a-b;
+}
+
+int mul(int a, int b) {
+  return a*b;
+}
+
+int div(int a, int b) {
+  return a/b;
+}
+
+void main() {
+  // int n = (add+div-mul) (12,2);
+  // printf("%d\n", n);
+  // int n = (add-mul+div) (12,2);
+  // printf("%d\n", n);
+
+  printf("%d %d", sizeof(double), sizeof(void*));
+}
+// gcc -s code.c
+// function pointerssss.
